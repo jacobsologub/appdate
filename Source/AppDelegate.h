127e686ce7a80a9a5ad2e95c38711fb6eb65dcca
Jacob Sologub <jacobsologub@gmail.com>
2014-06-08 13:34:55 -0700

    Updated copyright notice.

--- Source/AppDelegate.h
+++ Source/AppDelegate.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
    
-   Copyright (C) 2012 Jacob Sologub
+   Copyright (C) 2014 Jacob Sologub
    
    Permission is hereby granted, free of charge, to any person obtaining a copy of
    this software and associated documentation files (the "Software"), to deal in
